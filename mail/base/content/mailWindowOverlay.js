48d53017c9889908dd0bfb4998059968d7288005
Magnus Melin <mkmelin+mozilla@iki.fi>
2026-09-11 03:41:53 -0700

    Bug 2057025 - [comm-esr153] Make Save/Detach/Delete All still work when one attachment is already deleted. r=tobyp a=coreycb
    (cherry picked from commit f05f72fbe0dd946446787926bae13bdcb1c8049d)
    
    Differential Revision: https://phabricator.services.mozilla.com/D325351

--- mail/base/content/mailWindowOverlay.js
+++ mail/base/content/mailWindowOverlay.js
@@ -1902,16 +1902,15 @@ function addAttachmentToPopup(
     item.classList.add("notfound");
   }
 
-  const detached = attachment.isExternalAttachment;
-  const deleted = !attachment.hasFile;
-  const canDetach =
-    aboutMessage?.CanDetachAttachments() && !deleted && !detached;
-
-  if (deleted) {
-    // We can't do anything with a deleted attachment, so just return.
+  if (!attachment.hasFile) {
+    // A deleted attachment, or a detached one with a missing file. We can't do
+    // anything with it, so just return.
     item.disabled = true;
     return;
   }
+
+  const canDetach =
+    aboutMessage?.CanDetachAttachments() && !attachment.isExternalAttachment;
 
   // Create the "open" menu item
   let menuitem = document.createXULElement("menuitem");
@@ -1921,7 +1920,6 @@ function addAttachmentToPopup(
   );
   menuitem.setAttribute("label", getString("openLabel"));
   menuitem.setAttribute("accesskey", getString("openLabelAccesskey"));
-  menuitem.toggleAttribute("disabled", deleted);
   menuitem = menupopup.appendChild(menuitem);
 
   // Create the "save" menu item
@@ -1932,7 +1930,6 @@ function addAttachmentToPopup(
   );
   menuitem.setAttribute("label", getString("saveLabel"));
   menuitem.setAttribute("accesskey", getString("saveLabelAccesskey"));
-  menuitem.toggleAttribute("disabled", deleted);
   menuitem = menupopup.appendChild(menuitem);
 
   // Create the "detach" menu item
@@ -1966,7 +1963,6 @@ function addAttachmentToPopup(
     menuitem.setAttribute("oncommand", "this.attachment.openFolder();");
     menuitem.setAttribute("label", getString("openFolderLabel"));
     menuitem.setAttribute("accesskey", getString("openFolderLabelAccesskey"));
-    menuitem.toggleAttribute("disabled", !attachment.hasFile);
     menuitem = menupopup.appendChild(menuitem);
   }
 }
